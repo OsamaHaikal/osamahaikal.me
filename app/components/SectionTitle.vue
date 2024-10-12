7e637ba1c1a914c2ff0430924eb98429e5382573
Osama Haikal <ee.osamahaikal@gmail.com>
2024-10-12 21:16:01 +0300

    feat: add title comopnent

---
+++ app/components/SectionTitle.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <h2 class="section-title text-start underline">
+    {{ title }}
+  </h2>
+</template>
+
+<style scoped>
+.section-title {
+  color: #fff;
+  font-size: 24px;
+  font-weight: bold;
+  letter-spacing: 6px;
+  text-transform: uppercase;
+  text-decoration-color: #192034;
+  text-underline-offset: 10px;
+}
+</style>
